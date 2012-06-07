394d8a0515359a993e038572d4ca2ef9f98410ab
Lois <burg.lois@gmail.com>
2012-06-07 11:06:21 +0200

    Mise a la normw

--- server/srcs/diamond_tools.c
+++ server/srcs/diamond_tools.c
@@ -5,11 +5,17 @@
 ** Login   <burg_l@epitech.net>
 **
 ** Started on  Wed Jun  6 19:20:53 2012 lois burg
-** Last update Thu Jun  7 09:19:38 2012 lois burg
+** Last update Thu Jun  7 11:05:18 2012 lois burg
 */
 
 #include <stdlib.h>
 #include "diamond_generation.h"
+
+static void	add_avg(double *avg, double *sum, const double val)
+{
+  *avg += val;
+  *sum += 1;
+}
 
 double		avg_diamond(const int x, const int y, t_dmap *dmap, const int size)
 {
@@ -20,25 +26,13 @@ double		avg_diamond(const int x, const int y, t_dmap *dmap, const int size)
   avg = 0;
   sum = 0;
   if (x - half_step >= 0)
-    {
-      avg += dmap->map[y][x - half_step];
-      sum += 1;
-    }
+    add_avg(&avg, &sum, dmap->map[y][x - half_step]);
   if (y - half_step >= 0)
-    {
-      avg += dmap->map[y - half_step][x];
-      sum += 1;
-    }
+    add_avg(&avg, &sum, dmap->map[y - half_step][x]);
   if (x + half_step < size)
-    {
-      avg += dmap->map[y][x + half_step];
-      sum += 1;
-    }
+    add_avg(&avg, &sum, dmap->map[y][x + half_step]);
   if (y + half_step < size)
-    {
-      avg += dmap->map[y + half_step][x];
-      sum += 1;
-    }
+    add_avg(&avg, &sum, dmap->map[y + half_step][x]);
   avg /= sum;
   avg += (((double)rand() / (double)RAND_MAX) * 2 * RAND_RANGE) - RAND_RANGE;
   return (avg);
