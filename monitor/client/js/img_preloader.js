0798eb0e17a31ccf2e6f44b51062a705a694aeb7
glafontaine <geoffroy.lafontaine@gmail.com>
2012-06-12 15:19:19 +0200

    img_preloader.js

---
+++ monitor/client/js/img_preloader.js
@@ -0,0 +1,21 @@
+/**
+ * @author lafont_g
+ */
+
+function img_preloader() {
+    var i = 0;
+    imgLoad = new Image();
+    
+    paths = new Array();
+    paths[0] = "img/test1.jpg";
+    paths[1] = "img/test2.jpg";
+    paths[2] = "img/test3.jpg";
+    paths[3] = "img/test4.jpg";
+
+    /* Preloading images */
+    for (i = 0; i <= paths.length; ++i)
+    {
+	imgLoad.src = paths[i];
+    }
+    alert('Loading done.');
+}
