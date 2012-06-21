6809a91d149d558fe86de2206ebdd01ab53534db
root <root@pc-sylvia-r.(none)>
2012-06-21 18:13:55 +0200

    layer clear + connection client-> node + autoupdate + latency

--- monitor/server/bufferParse.js
+++ monitor/server/bufferParse.js
@@ -8,7 +8,7 @@ exports.feed = function(buffer, cache) {
 	var arrayBuffer = buffer.split("\n"),
 		current;
 		
-	console.log(buffer);
+	//console.log(buffer);
 	for (var i = 0; i != arrayBuffer.length; i++)
 	{
 		if (arrayBuffer[i] != "")
