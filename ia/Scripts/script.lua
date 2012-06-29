026400ebe027a197d6a887a555b3b697d317bfe1
Thomas Duplomb <duplom_t@epitech.eu>
2012-06-29 12:03:31 +0200

    create luascript file

---
+++ ia/Scripts/script.lua
@@ -0,0 +1,16 @@
+#!/usr/bin/lua
+
+function this.avance(this)
+	 this:IAAvance()
+	 return (0)
+end
+
+function this.voir(this)
+	 this:IAVoir()
+	 return (0)
+end
+
+function this.prendre(this)
+	 this:IAPrendre("nourriture")
+	 return (0)
+end
