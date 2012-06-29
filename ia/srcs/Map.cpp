70d79183b5fe613b719b94e622307ee9db26d5dd
Loadex <carpen_t@epitech.eu>
2012-06-29 11:16:12 +0200

    debut de map

--- ia/srcs/Map.cpp
+++ ia/srcs/Map.cpp
@@ -5,10 +5,14 @@
 // Login   <carpen_t@epitech.net>
 //
 // Started on  Mon Jun 25 13:50:12 2012 thibault carpentier
-// Last update Wed Jun 27 14:16:25 2012 thibault carpentier
+// Last update Fri Jun 29 10:59:38 2012 thibault carpentier
 //
 
+#include <boost/regex.hpp>
 #include "Map.hh"
+#include "TrantorienFailure.hh"
+
+std::string const Map::REGEX_VALUE = " *\\{( *(joueur|linemate|deraumere|sibur|mendiane|phiras|thystame|nourriture) *([ ,]*|\\} *$){1} *)+ *\\} *";
 
 Map::Map(position mapsize)
   : mapsize_(mapsize), offset_(std::pair<int, int>(0, 0)), currentOrientation_(EST), currentPos_(0, 0)
@@ -65,8 +69,89 @@ void Map::avancer(void)
   //  std::cout << currentPos_.first << " " << currentPos_.second << std::endl;
 }
 
+// temporary
 #include <iostream>
+void Map::analyse(const std::string &values)
+{
+  size_t beginCase = 0, tmp;
+
+  while ((tmp = values.find(",", beginCase)) != std::string::npos)
+    {
+      std::string caseSee(values, beginCase, tmp);
+      std::cout <<"tmp = " << tmp <<"result : "<< caseSee << std::endl;
+      beginCase = tmp + 1;
+    }
+}
+
 void Map::voir(const std::string &values)
 {
-  std::cout << values << std::endl;
+  // temporary
+  boost::regex regex(REGEX_VALUE);
+
+  if (boost::regex_match(values, regex))
+    {
+      std::cout << values << std::endl;
+      analyse(values);
+    }
+  else
+    throw TrantorienFailure("Map voir", "Received " + values + " witch does not match with the regex " + REGEX_VALUE);
 }
+
+
+
+//" *{( *(joueur|linemate|nourriture) *([ ,]*|} *$){1} *)+ *} *"
+
+//         _____ _____ _____ _____ _____
+//        |     |     |     |     |     |
+//        |-2 -2|-1 -2|-0 -2|+1 -2|+2 -2|
+//        |_____|_____|_____|_____|_____|
+//              |     |     |     |
+//              |-1 -1|-0 -1|+1 -1|
+//              |_____|_____|_____|
+//                    |     |
+//                    |+0 +0|
+//                    |_____|
+//  _____                                _____
+// |     |                              |     |
+// |-2 +2|                              |+2 +2|
+// |_____|_____                    _____|_____|
+// |     |     |                  |     |     |
+// |-2 +1|-1 +1|                  |+1 +1|+2 +1|
+// |_____|_____|_____        _____| ____|_____|
+// |     |     |     |      |     |     |     |
+// |-2 -0|-1 -0|+0 +0|      |+0 +0|+1 +0|+2 +0|
+// |_____|_____|_____|      |_____|_____|_____|
+// |     |     |                  |     |     |
+// |-2 -1|-1 -1|                  |+1 -1|+2 -1|
+// |_____|_____|                  |_____|_____|
+// |     |                              |     |
+// |-2 -2|                              |+2 -2|
+// |_____|                              |_____|
+//                     _____
+//                    |     |
+//                    |+0 +0|
+//               _____|_____|_____
+//              |     |     |     |
+//              |-1 +1|+0 +1|-1 +1|
+//         _____|_____|_____|_____|_____
+//        |     |     |     |     |     |
+//        |-2 +2|-1 +2|-0 +2|+1 +2|+2 +2|
+//        |_____|_____|_____|_____|_____|
+
+// nbcase = 1;
+// distance = +?- 0
+// incrstart = 1 - 1 / 2
+// nbcase += 2
+//
+
+
+//  __ __ __ __ __
+// |  |  |  |  |  |
+// | 4| 5| 6| 7| 8|
+// |__|__|__|__|__|
+//    |  |  |  |
+//    | 1| 2| 3|
+//    |__|__|__|
+//       |  |
+//       | 0|
+//       |__|
