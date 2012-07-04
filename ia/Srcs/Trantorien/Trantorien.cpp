6d0427d1222ac7a127e91c0a5c7f6c58205aa4db
Thomas Duplomb <duplom_t@epitech.eu>
2012-07-04 16:47:57 +0200

    trucs

--- ia/Srcs/Trantorien/Trantorien.cpp
+++ ia/Srcs/Trantorien/Trantorien.cpp
@@ -72,7 +72,6 @@ int Trantorien::voir(LuaVirtualMachine::VirtualMachine &vm)
   if (ret != "ko")
     map_.voir(ret);
   map_.test();
-  abort();
   return 0;
 }
 
@@ -90,11 +89,12 @@ int Trantorien::inventaire(LuaVirtualMachine::VirtualMachine &vm)
 
 int Trantorien::prendre(LuaVirtualMachine::VirtualMachine &vm)
 {
-  lua_State *state = vm.getLua();
+  lua_State               *state = vm.getLua();
+  std::list<std::string>  answers;
   int i = 1;
 
-//  for (i = 1; i <= lua_gettop(state); ++i)
-//    {
+  for (i = 1; i <= lua_gettop(state); ++i)
+    {
       if (lua_isstring(state, i))
         {
           std::string object(lua_tostring(state, i));
@@ -102,10 +102,17 @@ int Trantorien::prendre(LuaVirtualMachine::VirtualMachine &vm)
           std::string result = network_.getline();
           if (result == "ok")
             inventory_.prendre(object);
-          lua_pushstring(state, result.c_str());
+          answers.push_back(result);
         }
-//    }
-  return (1);
+    }
+  i = 0;
+  while (!answers.empty())
+    {
+      lua_pushstring(state, answers.front().c_str());
+      answers.pop_front();
+      ++i;
+    }
+  return (i);
 }
 
 int Trantorien::tourner(LuaVirtualMachine::VirtualMachine &vm)
