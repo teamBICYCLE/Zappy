52399a54cfcb54efafe146a4c38ee3654eb47c6e
Lois <burg.lois@gmail.com>
2012-07-09 11:41:44 +0200

    Fix d'un bug dans graphics_pic: j'envoyais deux fois le meme id

--- server/srcs/graphic_response2.c
+++ server/srcs/graphic_response2.c
@@ -5,7 +5,7 @@
 ** Login   <burg_l@epitech.net>
 **
 ** Started on  Sat Jun 16 11:43:52 2012 lois burg
-** Last update Mon Jul  9 11:26:50 2012 lois burg
+** Last update Mon Jul  9 11:41:09 2012 lois burg
 */
 
 #include <string.h>
@@ -67,6 +67,7 @@ char		*graphics_pic(t_users *usr)
   uint		i;
   int		idx;
   t_link	*plyr;
+  t_users	*p;
   char		*msg;
 
   msg = get_graphics_buf();
@@ -75,11 +76,13 @@ char		*graphics_pic(t_users *usr)
   i = 0;
   while (i < g_info.users->size)
     {
-      if ((plyr = get_link(g_info.users, i)) &&
-	  ((t_users*)plyr->ptr)->x == usr->x &&
-	  ((t_users*)plyr->ptr)->y == usr->y)
-	idx += snprintf(msg + idx, GRAPHICS_MSG_SZ - idx, " %d",
-			((t_users*)plyr->ptr)->id);
+      if ((plyr = get_link(g_info.users, i)))
+	{
+	  p = (t_users*)plyr->ptr;
+	  if (p->type != TGRAPHICS && p->id != usr->id &&
+	      p->x == usr->x && p->y == usr->y)
+	    idx += snprintf(msg + idx, GRAPHICS_MSG_SZ - idx, " %d", p->id);
+	}
       ++i;
     }
   snprintf(msg + idx, GRAPHICS_MSG_SZ - idx, "\n");
