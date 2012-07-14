c4a716fd3bfb118970c7648873d9b8eacd6b3423
Lois <burg.lois@gmail.com>
2012-07-14 12:41:38 +0200

    Fix du parcours de la carte des see_count

--- server/srcs/see_utils_count.c
+++ server/srcs/see_utils_count.c
@@ -5,7 +5,7 @@
 ** Login   <burg_l@epitech.net>
 **
 ** Started on  Thu Jun 14 14:33:04 2012 lois burg
-** Last update Sat Jul 14 12:34:38 2012 lois burg
+** Last update Sat Jul 14 12:40:43 2012 lois burg
 */
 
 #include "cmds.h"
@@ -78,8 +78,8 @@ size_t		see_count_south(const int usr_x, const int usr_y,
   lvl = 0;
   while (lvl <= usr_lvl)
     {
+      x = ((usr_x + lvl) + map->x) % map->x;
       y = ((usr_y + lvl) + map->y) % map->y;
-      x = ((usr_x - lvl) + map->x) % map->x;
       cases_left = (lvl * 2) + 1;
       while (cases_left)
 	{
@@ -106,7 +106,7 @@ size_t		see_count_west(const int usr_x, const int usr_y,
   while (lvl <= usr_lvl)
     {
       x = ((usr_x - lvl) + map->x) % map->x;
-      y = ((usr_y - lvl) + map->y) % map->y;
+      y = ((usr_y + lvl) + map->y) % map->y;
       cases_left = (lvl * 2) + 1;
       while (cases_left)
 	{
